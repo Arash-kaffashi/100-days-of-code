1380bb5eaf48ad7531687ebba3656c53672087ae
Arash Kaffashi <arash_kaffashi@hotmail.com>
2023-01-31 19:13:07 -0400

    implement: tests

--- Month-1/Week-01/day-01/convert-age/src/main.rs
+++ Month-1/Week-01/day-01/convert-age/src/main.rs
@@ -7,3 +7,37 @@ fn main() {
             * 365
     );
 }
+
+#[test]
+fn validade_success() {
+    let output = std::process::Command::new("cargo")
+        .arg("run")
+        .arg("10")
+        .output()
+        .unwrap();
+    assert!(output.status.success());
+    assert_eq!(output.stdout, b"You are 3650 days old.\n");
+}
+
+#[test]
+fn validate_empty_year() {
+    let output = std::process::Command::new("cargo")
+        .arg("run")
+        .output()
+        .unwrap();
+    assert!(!output.status.success());
+    let res = b"Expected one argument.";
+    assert!(output.stderr.windows(res.len()).any(|window| window == res));
+}
+
+#[test]
+fn validate_invalid_year() {
+    let output = std::process::Command::new("cargo")
+        .arg("run")
+        .arg("ten")
+        .output()
+        .unwrap();
+    assert!(!output.status.success());
+    let res = b"Expected argument to be a number.";
+    assert!(output.stderr.windows(res.len()).any(|window| window == res));
+}
